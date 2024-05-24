6c6fbfc86ea8d550e791a05f836ed0bd4c15ab28
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-05-24 10:52:18 +0200

    Create a 'productsData object, consolidate product names into single string & log it

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
+/*       Exercises       */
+
 // A list of provinces:
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+const provinces = [
+  "Western Cape",
+  "Gauteng",
+  "Northern Cape",
+  "Eastern Cape",
+  "KwaZulu-Natal",
+  "Free State",
+];
 
 // A list of names:
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
-
-// A list of products with prices:
-const products = [
-  { product: 'banana', price: "2" },
-  { product: 'mango', price: 6 },
-  { product: 'potato', price: ' ' },
-  { product: 'avocado', price: "8" },
-  { product: 'coffee', price: 10 },
-  { product: 'tea', price: '' },
-]
+const names = [
+  "Ashwin",
+  "Sibongile",
+  "Jan-Hendrik",
+  "Sifso",
+  "Shailen",
+  "Frikkie",
+];
 
 // forEach() Basics: Logs each name and each province to the console. Then, logs each name with a matching province in the format "Name (Province)".
 provinces.forEach((province) => console.log(province));
@@ -46,3 +52,23 @@ const nameProvinceMap = names.reduce((acc, name, index) => {
   return acc;
 }, {});
 console.log(nameProvinceMap);
+
+
+/*       Advanced Exercises       */
+
+// A list of products with prices:
+const products = [
+  { product: "banana", price: "2" },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: "8" },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
+];
+
+const productsData = {
+  // Iterates over the products array & logs each product name
+  productName: products.map((name) => name.product).join(", "),
+};
+
+console.log(productsData);
